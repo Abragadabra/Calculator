e03282a3f7cd60262219f0c974208c4dbbbba5da
Alexander <77795206+Abragadabra@users.noreply.github.com>
2021-01-22 20:57:06 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@
 using namespace std;
 
 int main() {
-   
+	int action, first, second, result;
+	cout << "Welcome to Calculator by Abragadarba" << endl;
+
+	cout << "What action are we performing? (addition [0], subtraction [1], division [2], multiplication [3]): ";
+	cin >> action; 
+
+	if (action == 0) {
+		cout << "Enter the first number: ";
+		cin >> first;
+
+		cout << "Enter the second number: ";
+		cin >> second;
+
+		result = first + second;
+
+		cout << "Sum of numbers: " << result << endl;
+	}
+	system("pause");
 }
